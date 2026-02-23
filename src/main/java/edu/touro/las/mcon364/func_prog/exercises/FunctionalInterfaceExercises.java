2ec93ef21d2e0680e7989bb357e3913fbe99edf9
tehillasebrow <tsebrow@student.touro.edu>
2026-02-22 22:03:39 -0500

    finished the classwork-it took over an hour!

--- src/main/java/edu/touro/las/mcon364/func_prog/exercises/FunctionalInterfaceExercises.java
+++ src/main/java/edu/touro/las/mcon364/func_prog/exercises/FunctionalInterfaceExercises.java
@@ -1,8 +1,10 @@
 package edu.touro.las.mcon364.func_prog.exercises;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -11,15 +13,15 @@ import java.util.function.Supplier;
 
 /**
  * Functional Interface Practice
- *
+ * <p>
  * In this assignment you will:
- *  - Create and return different functional interfaces
- *  - Apply them
- *  - Practice chaining where appropriate
- *
+ * - Create and return different functional interfaces
+ * - Apply them
+ * - Practice chaining where appropriate
+ * <p>
  * IMPORTANT:
- *  - Use lambdas
- *  - Do NOT use anonymous classes
+ * - Use lambdas
+ * - Do NOT use anonymous classes
  */
 public class FunctionalInterfaceExercises {
 
@@ -29,19 +31,19 @@ public class FunctionalInterfaceExercises {
 
     /**
      * 1) Create a Supplier that returns the current year.
-     *
+     * <p>
      * Hint:
      * You can get the current date using:
-     *     LocalDate.now()
-     *
+     * LocalDate.now()
+     * <p>
      * Then extract the year using:
-     *     getYear()
-     *
+     * getYear()
+     * <p>
      * Example (not the solution):
      *
      */
     public static Supplier<Integer> currentYearSupplier() {
-      // TODO
+        // TODO
         return () -> LocalDate.now().getYear();
     }
 
@@ -63,18 +65,18 @@ public class FunctionalInterfaceExercises {
      * a string is all uppercase.
      */
     public static Predicate<String> isAllUpperCase() {
-        return s-> Objects.equals(s, s.toUpperCase());
+        return s -> Objects.equals(s, s.toUpperCase());
     }
 
     /**
      * 4) Create a Predicate that checks whether
      * a number is positive AND divisible by 5.
-     *
+     * <p>
      * Hint: consider chaining.
      */
     public static Predicate<Integer> positiveAndDivisibleByFive() {
         // TODO
-        return  num->(num>0 && num % 5==0);
+        return num -> (num > 0 && num % 5 == 0);
     }
 
     // =========================================================
@@ -84,29 +86,36 @@ public class FunctionalInterfaceExercises {
     /**
      * 5) Create a Function that converts
      * a temperature in Celsius to Fahrenheit.
-     *
+     * <p>
      * Formula: F = C * 9/5 + 32
      */
     public static Function<Double, Double> celsiusToFahrenheit() {
         // TODO
 
-        return temp->(temp *(9/5)+ 32);
+        return temp -> (temp * (9 / 5) + 32);
     }
 
     /**
      * 6) Create a Function that takes a String
      * and returns the number of vowels in it.
-     *
+     * <p>
      * Bonus: Make it case-insensitive.
      */
     public static Function<String, Integer> countVowels() {
         // TODO
-       String str="";
-        String vowels="aeiou";
-        for(int i=0; i<str.length();i++ ){
-            if (str.indexOf(i).)
-        }
-        return null;
+
+        return (String s) -> {
+            s = s.toLowerCase();
+            String vowels = "aeiou";
+            int ctr = 0;
+            for (int i = 0; i < s.length(); i++) {
+                String c = String.valueOf(s.charAt(i));
+                if (vowels.contains(c))
+                    ctr++;
+            }
+
+            return ctr;
+        };
     }
 
     // =========================================================
@@ -116,13 +125,13 @@ public class FunctionalInterfaceExercises {
     /**
      * 7) Create a Consumer that prints a value
      * surrounded by "***"
-     *
+     * <p>
      * Example output:
      * *** Hello ***
      */
     public static Consumer<String> starPrinter() {
         // TODO
-        return null;
+        return (value) -> System.out.println("*** " + value + " ***");
     }
 
     /**
@@ -131,7 +140,7 @@ public class FunctionalInterfaceExercises {
      */
     public static Consumer<Integer> printSquare() {
         // TODO
-        return null;
+        return (num) -> System.out.println(num * num);
     }
 
     // =========================================================
@@ -140,29 +149,60 @@ public class FunctionalInterfaceExercises {
 
     /**
      * 9) Apply:
-     *  - A Predicate
-     *  - A Function
-     *  - A Consumer
-     *
+     * - A Predicate
+     * - A Function
+     * - A Consumer
+     * <p>
      * Process the list as follows:
-     *  - Keep only strings longer than 3 characters
-     *  - Convert them to lowercase
-     *  - Print them
+     * - Keep only strings longer than 3 characters
+     * - Convert them to lowercase
+     * - Print them
      */
     public static void processStrings(List<String> values) {
         // TODO
+
+        Predicate<String> stringsOfThree = (s) -> s.length() >= 3;
+        Function<String, String> lowerCase = String::toLowerCase;
+        Consumer<List<String>> printS = v-> {
+            for (String str :v) {
+                if (stringsOfThree.test(str)) {
+                    str = lowerCase.apply(str);
+                    System.out.println(str);
+                }
+            }
+        };
+        printS.accept(values);
     }
+
+
+
 
     /**
      * 10) Apply:
-     *  - A Supplier
-     *  - A Predicate
-     *  - A Consumer
-     *
+     * - A Supplier
+     * - A Predicate
+     * - A Consumer
+     * <p>
      * Generate 5 random scores.
      * Print only those above 70.
      */
     public static void generateAndFilterScores() {
         // TODO
+        Supplier<List<Integer>> numGenerator= ()-> {
+            List<Integer>list= new ArrayList<>();
+            Random random = new Random();
+            for(int i=0;i<4;i++){
+            list.add(random.nextInt(101));
+            }
+            return list;};
+        Predicate<Integer> over70= i->i>70;
+        Consumer<List<Integer>> printAbove70= list->{
+            for(Integer integer: list){
+                if(over70.test(integer))
+                    System.out.println(integer);
+            }
+        };
+        printAbove70.accept(numGenerator);
     }
 }
+
